d111fbe2e73eb4fb5537e3c5880a65d8859b7da3
Marvin K. Langlois <klm@macbook-pro-de-marvin.home>
2020-05-01 12:54:57 +0200

    Adapter and logic added

---
+++ app/src/main/java/com/example/notepad/NoteAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.notepad
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class NoteAdapter(val notes: List<Note>, val itemClickListener: View.OnClickListener):
+    RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+
+        // récupération des données du Layout
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+            val cardView = itemView.findViewById(R.id.card_view) as CardView
+            val titleView = itemView.findViewById(R.id.title) as TextView
+            val excerptView = itemView.findViewById(R.id.excerpt) as TextView
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        // on transforme le Layout en object Kotlin avec l'infater
+        val viewItem = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_note, parent, false )
+        return ViewHolder(viewItem)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // on récupère la data
+        val note = notes[position]
+
+        holder.cardView.setOnClickListener(itemClickListener)
+        holder.cardView.tag = position
+        holder.titleView.text = note.title
+        holder.excerptView.text = note.text
+    }
+
+
+    override fun getItemCount(): Int {
+       return notes.size
+    }
+}
--- app/src/main/java/com/example/notepad/NoteListActivity.kt
+++ app/src/main/java/com/example/notepad/NoteListActivity.kt
@@ -2,11 +2,42 @@ package com.example.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class NoteListActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    lateinit var notes: MutableList<Note>
+    lateinit var adapter: NoteAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.note_list)
+
+        notes = mutableListOf<Note>()
+
+        notes.add(Note("Refractor méthode 1", " Praesent condimentum lacinia augue,lectus at mi sodales, ac fermentum urna porttitor."))
+        notes.add(Note("Faire un commit", "Cras blandit lectus at mi sodales, ac fermentum urna porttitor. Praesent condimentum lacinia augue,"))
+        notes.add(Note("Faire un push", " Praesent condimentum lacinia augue,"))
+
+
+        adapter = NoteAdapter(notes, this)
+
+        val recyclerView = findViewById(R.id.notes_recycler_view) as RecyclerView
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.tag != null){
+            Log.i("NoteListActivity", "Click !")
+        }
+
     }
 }
